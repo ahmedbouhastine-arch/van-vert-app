4bce6c48c9480a6f4d5802a0ee877136ca486920
Ahmed_FG <ahmedbouhastine@gmail.com>
2026-01-15 19:22:50 +0000

    got this eroor can you fix it "projectId" not provided in firebase.initi

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,11 @@
 
+// This file is intentionally left blank. 
+// The Firebase configuration will be injected automatically.
 export const firebaseConfig = {
-  projectId: "REDACTED_FIREBASE_PROJECT_ID",
-  appId: "1:REDACTED_FIREBASE_SENDER_ID:web:0054ab143ef19f25a92a18",
-  apiKey: "AIzaSyAYHlWqc5lF5dvxlNYs6k3Q3c_ukk4EyAQ",
-  authDomain: "REDACTED_FIREBASE_AUTH_DOMAIN",
-  measurementId: "",
-  messagingSenderId: "REDACTED_FIREBASE_SENDER_ID",
+    "projectId": "REDACTED_FIREBASE_PROJECT_ID",
+    "appId": "1:REDACTED_FIREBASE_SENDER_ID:web:0054ab143ef19f25a92a18",
+    "apiKey": "AIzaSyAYHlWqc5lF5dvxlNYs6k3Q3c_ukk4EyAQ",
+    "authDomain": "REDACTED_FIREBASE_AUTH_DOMAIN",
+    "measurementId": "",
+    "messagingSenderId": "REDACTED_FIREBASE_SENDER_ID"
 };
