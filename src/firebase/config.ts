6b61ab93cce6c8f43750319c1ab13365ac8bf83d
Ahmed_FG <ahmedbouhastine@gmail.com>
2026-01-15 12:36:14 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,4 +1,9 @@
 
-// This file is intentionally left blank. 
-// The firebaseConfig will be populated by the Firebase Admin SDK.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  projectId: "studio-5434730977-5c28e",
+  appId: "1:157949929417:web:0054ab143ef19f25a92a18",
+  apiKey: "AIzaSyAYHlWqc5lF5dvxlNYs6k3Q3c_ukk4EyAQ",
+  authDomain: "studio-5434730977-5c28e.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "157949929417",
+};
