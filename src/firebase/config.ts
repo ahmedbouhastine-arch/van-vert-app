f3e49da87e8b8b79f548299dae07c7c4d23ee737
Ahmed_FG <ahmedbouhastine@gmail.com>
2026-02-06 12:39:19 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,12 @@
+
+// This file is intentionally left blank. 
+// The Firebase configuration will be injected automatically.
 export const firebaseConfig = {
-  "projectId": "pilotpack-pre-42167387-6c831",
-  "appId": "1:444410678961:web:a67dde9c7099cdfafa9c90",
-  "apiKey": "AIzaSyCaeKxM0WkR0SY3CmA0v_Scpn660rwgBHQ",
-  "authDomain": "pilotpack-pre-42167387-6c831.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "444410678961"
+    "apiKey":"AIzaSyCaeKxM0WkR0SY3CmA0v_Scpn660rwgBHQ",
+    "appId":"1:444410678961:web:a67dde9c7099cdfafa9c90",
+    "authDomain":"pilotpack-pre-42167387-6c831.firebaseapp.com",
+    "measurementId":"",
+    "messagingSenderId":"444410678961",
+    "projectId":"pilotpack-pre-42167387-6c831"
 };
+
