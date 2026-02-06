3413f39dd977ccb9db6ebc7959e23dccd34ffe8b
Ahmed_FG <ahmedbouhastine@gmail.com>
2026-02-06 12:38:58 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,8 @@
-
-// This file is intentionally left blank. 
-// The Firebase configuration will be injected automatically.
 export const firebaseConfig = {
-    "projectId": "studio-5434730977-5c28e",
-    "appId": "1:157949929417:web:0054ab143ef19f25a92a18",
-    "apiKey": "AIzaSyAYHlWqc5lF5dvxlNYs6k3Q3c_ukk4EyAQ",
-    "authDomain": "studio-5434730977-5c28e.firebaseapp.com",
-    "measurementId": "",
-    "messagingSenderId": "157949929417"
+  "projectId": "pilotpack-pre-42167387-6c831",
+  "appId": "1:444410678961:web:a67dde9c7099cdfafa9c90",
+  "apiKey": "AIzaSyCaeKxM0WkR0SY3CmA0v_Scpn660rwgBHQ",
+  "authDomain": "pilotpack-pre-42167387-6c831.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "444410678961"
 };
