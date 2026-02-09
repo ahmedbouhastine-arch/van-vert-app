c93337169f51e92ae10416b615bf5f4428639177
Ahmed_FG <ahmedbouhastine@gmail.com>
2026-02-09 17:35:21 +0000

    early-page.js:796  POST https://identitytoolkit.googleapis.com/v1/accou

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,11 +2,11 @@
 // This file is intentionally left blank. 
 // The Firebase configuration will be injected automatically.
 export const firebaseConfig = {
-    "apiKey":"AIzaSyCaeKxM0WkR0SY3CmA0v_Scpn660rwgBHQ",
-    "appId":"1:444410678961:web:a67dde9c7099cdfafa9c90",
-    "authDomain":"van-vert-app--studio-5434730977-5c28e.firebaseapp.com",
+    "apiKey":"AIzaSyAYHlWqc5lF5dvxlNYs6k3Q3c_ukk4EyAQ",
+    "appId":"1:157949929417:web:d957027b890f102da92a18",
+    "authDomain":"studio-5434730977-5c28e.firebaseapp.com",
     "measurementId":"G-1WJ4K132Z7",
-    "messagingSenderId":"444410678961",
+    "messagingSenderId":"157949929417",
     "projectId":"studio-5434730977-5c28e",
     "storageBucket": "studio-5434730977-5c28e.appspot.com"
 };
