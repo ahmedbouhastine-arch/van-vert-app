a03f3a1fa96f8b5c83f5f5c8fa23011e1d5ded2f
Ahmed_FG <ahmedbouhastine@gmail.com>
2026-02-16 17:17:58 +0000

    Function updateDoc() called with invalid data. Unsupported field value:

---
+++ workspace/src/next.config.ts
@@ -0,0 +1,48 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  allowedDevOrigins: ["https://3000-firebase-pilotpack-pre-1769026686788.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev"],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '50mb',
+      allowedOrigins: ["https://3000-firebase-pilotpack-pre-1769026686788.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev"],
+    },
+  },
+};
+
+export default nextConfig;
