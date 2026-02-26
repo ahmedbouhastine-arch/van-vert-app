c90d6c4c279c71acdd8886c365ae61988a79166d
Ahmed_FG <ahmedbouhastine@gmail.com>
2026-02-26 00:10:21 +0000

    Fix(firebase): Make Firebase config available at runtime

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,13 +1,9 @@
-const webAppConfig = process.env.FIREBASE_WEBAPP_CONFIG
-  ? JSON.parse(process.env.FIREBASE_WEBAPP_CONFIG)
-  : {};
-
 export const firebaseConfig = {
-  apiKey: webAppConfig.apiKey ?? process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: webAppConfig.authDomain ?? process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: webAppConfig.projectId ?? process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: webAppConfig.storageBucket ?? process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: webAppConfig.messagingSenderId ?? process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: webAppConfig.appId ?? process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: webAppConfig.measurementId ?? process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
